1942fb3eed428ff413e13fce65ba50e99606770f
Roberto Eustaquio Jiménez <reusjimenez2002@gmail.com>
2024-08-30 17:36:06 -0500

    refactor: improve code readability and optimize functions

--- util.py
+++ util.py
@@ -1,12 +1,3 @@
-'''
-GRUPO #3:
-- Durand Azurza, Mercedes Anayely
-- Echevarria Otazo, Kenner Alexander
-- Eustaquio Jiménez, Roberto Edmundo
-- Fernandez Perez, Kiara Luz
-- Laura Abarca, Herber
-'''
-
 # definir constantes
 ADD = 1
 UPD = 2
